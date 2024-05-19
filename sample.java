919f609378b5708a62bcc3c79a0959fab392cff4
mohammadalbasis <44113582+mohammadalbasis@users.noreply.github.com>
2024-05-19 17:37:47 +0200

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,2 +1,13 @@
-#my second file to my repo
-  #java code goes hier
+ //my jenkins sample java  file
+ class HelloWorld
+
+   {
+     public static void main(String[] arge)
+     {
+       System.out.println("Java sample code that run from jenkins.")
+
+         }
+
+   };
+   //execute java file today with the build step.
+   // this is the demo before adding build step.
